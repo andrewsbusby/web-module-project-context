56f902ac66f65e7b2d66168282252123dc41fe1c
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-06-10 15:18:42 -0600

    working on step 4

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import data from './data';
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
-import ProductContext from './context/ProductContext';
+import {ProductContext} from './context/ProductContext';
 
 function App() {
 	const [products] = useState(data);
@@ -24,7 +24,7 @@ function App() {
 
 			{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products />
 				</Route>
 
 				<Route path="/cart">
